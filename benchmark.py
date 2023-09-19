7ac783e04676d633291499adada5f0fc0802a4c1
theylovecjawn <cjohn@brooklynsteamcenter.com>
2023-09-19 09:50:44 -0400

    Python Exercises

---
+++ benchmark.py
@@ -0,0 +1,46 @@
+#Exercise 1:
+
+print("Caleb John")
+
+#Exercise 2:
+
+fav_show = "Greys Anatomy"
+
+#Exercise 3:
+
+height = 185.42
+age = 17
+
+print(height * age)
+
+#Exercise 4:
+
+print("i going to break")
+
+#Exercise 5:
+
+fav_food = ["Chips", "Candy", "Captain Crunch"]
+
+#Exercise 6:
+
+if age >= 21:
+    print("You an rent a car")
+else:
+    print("your'e beat...")
+
+#Exercise 7:
+
+
+their_name = input("Enter your name: ")
+print('Hello, ' + their_name)
+
+#Exercise 8:
+
+for i in fav_food:
+    print(i)
+
+#Exercise 9: 
+
+while age > 71:
+    print("WHOOOOOHOOOO")
+
