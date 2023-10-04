98aaa18f198e44b66712dae33265de988fadaf98
theylovecjawn <cjohn@brooklynsteamcenter.com>
2023-10-04 10:47:59 -0400

    done todos

--- todo.py
+++ todo.py
@@ -14,11 +14,14 @@ while True: #Runs once the code is ran
         new_todo = input('What is your new todo?: ') #Asks the user to type in the Todo they want.
         todos.append(new_todo) #adds to the end of the list
     elif user_input == "remove":#If the user types in remove this runs
-        remove_input = int(input("What would you like to remove? (give number of todo): " )) #Asks the uder for a number input to use to remove
-        if remove_input <= 0: #Makes sure that the number inputted is  greater than 0
-            print("Sorry You must input a number on the list.") #prints if this isnt greater than 0
-        else: #runs if the numbevr is greater than 0
-            del todos[remove_input - 1] #deletes the number on the list the uder inputs
+        if len(todos) == 0: #Makes sure that the list has no items in it already
+            print('Sorry you have no todos already.') #prints if there is no previous items in the list and they try to remove one
+        else:
+            remove_input = int(input("What would you like to remove? (give number of todo): " )) #Asks the uder for a number input to use to remove
+            if remove_input <= 0: #Makes sure that the number inputted is  greater than 0
+                print("Sorry You must input a number on the list.") #prints if this isnt greater than 0
+            else: #runs if the numbevr is greater than 0
+                del todos[remove_input - 1] #deletes the number on the list the uder inputs
  
 
     
